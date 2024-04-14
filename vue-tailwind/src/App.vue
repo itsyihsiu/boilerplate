2c1e0b2d9c5b08b374fa9c7b36d66178fe6c92f7
yihsiu <yihsiu.chen@icloud.com>
2024-04-14 13:17:05 +0800

    Add vue with tailwind

---
+++ vue-tailwind/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1>Hello, World</h1>
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  Button
+</button>
+</template>
+
+<style scoped>
+
+</style>
